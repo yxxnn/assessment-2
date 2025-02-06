fb573dbf875169cc6d993e2f0a831bddb253714a
yxxnn <s10267600@connect.np.edu.sg>
2025-02-06 21:54:24 +0800

    profile drop down menu

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ window.addEventListener('scroll', function() {
         header.classList.remove("shrink");
     }
 });
+
+function toggleDropdown() {
+    let dropdown = document.getElementById("dropdownMenu");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+
+// Close dropdown if clicked outside
+window.onclick = function(event) {
+    if (!event.target.matches('.profile-icon')) {
+        document.getElementById("dropdownMenu").style.display = "none";
+    }
+};
+
+function logoutUser() {
+    window.location.href = "Login.html"; 
+}
